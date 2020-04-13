49a7a0af3447f81179703d1fbb608eaafb3b5f8b
ricky1986 <ricky@class4ever.com>
2020-04-13 19:29:03 +0800

    0413

---
+++ admin_ui/admin/app.js
@@ -0,0 +1,34 @@
+//Vue
+const app = new Vue({
+    el: '#app',
+    data: { //数据
+        siteName: site.title,
+        menu: site.menu,
+        activeKey: 0
+    },
+    mounted: function () {
+        document.title = this.siteName
+    },
+    methods: {}
+})
+
+
+//jQuery
+$(document).ready(function () {
+    /**监听: 窗口resize */
+    function resizeWindow(e) {
+        var w = $(window).width()
+        console.log('新的是' + w)
+        $('.main-frame').width(w - 200)
+    }
+
+    /**主进程 */
+    function main() {
+        $(window).bind("resize", resizeWindow);
+        resizeWindow()
+    }
+
+    /**开始 */
+    main() //激活主进程
+
+})
