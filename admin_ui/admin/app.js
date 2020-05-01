dbaf19e3442a1177ffe8c0034d292087482b16a5
ricky1986 <ricky@class4ever.com>
2020-05-01 14:32:42 +0800

    1432

--- admin_ui/admin/app.js
+++ admin_ui/admin/app.js
@@ -1,33 +1,41 @@
+//取得frame
+function getFrame(uri) {
+    return `<iframe src="${uri}" class="main-frame"></iframe>`
+}
+
+
+//解析路由
+var routes = []
+for (let k in site.menu) {
+    let v = site.menu[k]
+    routes[k] = { path: `/${v.path}`, component: { template: getFrame(v.uri) } }
+}
+//设置404页面
+const notFound = { path: `*`, component: { template: getFrame('404.html') } }
+routes.push(notFound)
+
+const router = new VueRouter({
+    routes
+})
+
+
 //Vue
 const app = new Vue({
-    el: '#app',
     data: { //数据
         siteName: site.title,
         menu: site.menu,
-        activeKey: 0
     },
     mounted: function () {
         document.title = this.siteName
+
+        //如果默认首页路由不是空的,则跳转至第1个那个首页
+        if (site.menu[0].path.length && this.$route.path == '/') this.$router.push(`/home`)
     },
-    methods: {}
-})
-
-
-//jQuery
-$(document).ready(function () {
-    /**监听: 窗口resize */
-    function resizeWindow(e) {
-        var w = $(window).width()
-        console.log('新的是' + w)
-        $('.main-frame').width(w - 200)
-    }
-
-    /**主进程 */
-    function main() {
-        $(window).bind("resize", resizeWindow);
-        resizeWindow()
-    }
-
-    /**开始 */
-    main() //激活主进程
-})
+    methods: {
+        goto: function (key) {
+            let _path = '/' + this.menu[key].path
+            if (this.$route.path != _path) this.$router.push(_path)
+        }
+    },
+    router
+}).$mount('#app')
